39c2f9581a77faee6e605ef71996f93e0a8f8a7a
Cpt-Price <harsimransingh237@gmail.com>
2021-06-19 01:10:17 +0530

    Courses fetched from DB and rendered using Ejs

--- controllers/course.controller.js
+++ controllers/course.controller.js
@@ -6,7 +6,7 @@ exports.getCourses = function(req, res){
             console.log(err);
         }
         else{
-            res.render('../views/courses.ejs', {result: ans});
+            res.render('../views/courses.ejs', {results: ans});
         }
     })
 
--- models/course.model.js
+++ models/course.model.js
@@ -5,6 +5,18 @@ const CourseSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    description: {
+        type: String,
+        required: true
+    },
+    duration: {
+        type: String,
+        required: true
+    },
+    cost: {
+        type: Number,
+        required: true
+    },
     createdAt: {
         type: Date,
         default: Date.now
